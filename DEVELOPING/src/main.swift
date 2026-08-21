b853edaad9128032ba1de9286ad4096ecce51c18
Tony Toelle <tonytoelle@gmail.com>
2026-08-21 16:58:54 +0700

    style: force sidebar toggle button up by 20px

--- DEVELOPING/src/main.swift
+++ DEVELOPING/src/main.swift
@@ -5490,6 +5490,7 @@ struct MainEditorView: View {
             .help("Toggle Sidebar")
             .padding(.leading, 78) // After traffic lights (72px) + gap
             .padding(.top, 7)      // Vertically centered in 38px titlebar
+            .offset(y: -20)        // Dinaikan paksa 20px
         }
         .onChange(of: outerGeo.size.width) { oldWidth, newWidth in
             if newWidth < 580 && store.isSidebarVisible {
