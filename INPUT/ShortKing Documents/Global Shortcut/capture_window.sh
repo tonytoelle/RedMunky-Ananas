61f9a635e2d23080ee2dd275ce45900ece486e8d
Tony Toelle <tonytoelle@gmail.com>
2026-08-22 09:54:34 +0700

    feat: automatically open Preview's markup toolbar after capture_window screenshot

--- INPUT/ShortKing Documents/Global Shortcut/capture_window.sh
+++ INPUT/ShortKing Documents/Global Shortcut/capture_window.sh
@@ -4,4 +4,23 @@
 filepath="$HOME/Desktop/Screenshot $(date +%Y-%m-%d_at_%H.%M.%S).png"
 
 # Capture interactive window and open in Preview if successful
-screencapture -i -w "$filepath" && open -a Preview "$filepath"
+if screencapture -i -w "$filepath"; then
+    open -a Preview "$filepath"
+    sleep 0.5
+    osascript -e '
+    tell application "System Events"
+        tell process "Preview"
+            set frontmost to true
+            try
+                if exists menu item "Show Markup Toolbar" of menu 1 of menu bar item "View" of menu bar 1 then
+                    click menu item "Show Markup Toolbar" of menu 1 of menu bar item "View" of menu bar 1
+                else
+                    keystroke "a" using {command down, shift down}
+                end if
+            on error
+                keystroke "a" using {command down, shift down}
+            end try
+        end tell
+    end tell
+    '
+fi
