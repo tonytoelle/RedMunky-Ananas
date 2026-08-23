b96ef17f78c13e963a59b41e950b1927ad4b64c5
Tony Toelle <tonytoelle@gmail.com>
2026-08-23 15:04:57 +0700

    fix: map only 2 initial points in DraggableActionListView when opening windowTransform editor

--- DEVELOPING/src/Views/DraggableActionListView.swift
+++ DEVELOPING/src/Views/DraggableActionListView.swift
@@ -245,11 +245,10 @@ struct ActionCardView: View {
                                     onSave()
                                 }
                             }
-                    case .windowTransform(let p1, let p2, let p3, _):
+                    case .windowTransform(let p1, _, let p3, _):
                         Button(action: {
-                            let initialPoints: [SequencePoint] = (p1 == .zero && p2 == .zero) ? [] : [
+                            let initialPoints: [SequencePoint] = (p1 == .zero && p3 == .zero) ? [] : [
                                 SequencePoint(point: p1, type: .click),
-                                SequencePoint(point: p2, type: .click),
                                 SequencePoint(point: p3, type: .click)
                             ]
                             CaptureOverlayWindow.shared = CaptureOverlayWindow(mode: .windowTransform(initialPoints: initialPoints)) { np1, np2, np3, np4 in
